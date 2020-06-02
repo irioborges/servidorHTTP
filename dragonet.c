26d16c6384f70f060de4854ad5f540843b76e015
Irio Borges <iriormb@gmail.com>
2020-06-02 12:33:19 -0300

    Adicionado socket

--- dragonet.c
+++ dragonet.c
@@ -1,13 +1,78 @@
-#include <stdio.h>
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
 #include <pthread.h>
-#include <sys/socket.h>
 
 
+#define PORT 80
 
 
 int main(int argc, char **argv){
 
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "<h1>Oi Roner, meu filho.</h1>"; 
+    FILE *fp;
+    char buffer2[1000];
 
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 80
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    while(1){
+
+    if (listen(server_fd, 3) < 0) { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  (socklen_t*)&addrlen))<0) { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    valread = read( new_socket , buffer, 1024); 
+
+    fp = fopen("htdocs/index.html", "r");
+
+    if(fp == NULL){
+        printf("Erro de abertura no arquivo\n");
+    }
+
+    while(fgets(buffer2, 20, fp) != NULL)
+        send(new_socket , buffer2 , strlen(buffer2) , 0 ); 
+
+    printf("%s\n",buffer ); 
+    
+    
+
+    }
 
     return 0;
+
 }
